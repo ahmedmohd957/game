0a91e4f0fb2cfca4b4e640c881fdfe6ba3bd88f6
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2022-03-07 17:10:09 +0100

    updating the project

--- dice/main.py
+++ dice/main.py
@@ -1,10 +1,5 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+def main():
+    print("cuz")
 
 
-p1 = Person("John", 36)
-
-print(p1.name)
-print(p1.age)
+main()
