eb09917beb6f2fc376d6d727c843281cbdc3289e
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2022-03-07 18:07:45 +0100

    adding random to dice

--- dice_game/dice.py
+++ dice_game/dice.py
@@ -1,1 +1,10 @@
-print("dice")
+import random
+
+
+class Dice:
+    def __init__(self):
+        random.seed()
+
+    def roll_dice():
+        rm = random.randrange(1, 6)
+        print("random is "+rm)
