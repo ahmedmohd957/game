05a868fa83c70d688dc65b14d35a6c3fbe800c6e
ahmedmohd957 <ahmed.mohd957@gmail.com>
2022-03-08 13:09:41 +0100

    updated player

--- dice_game/player.py
+++ dice_game/player.py
@@ -1,9 +1,10 @@
 class Player:
-    
+
+    name = ""
     score = 0
 
-    def __init__(self, name):
-        self.name = name
+    def setName(self):
+        self.name = input("Enter your name: ")
 
     def updateName(self):
         self.name = input("Enter a new name: ")
