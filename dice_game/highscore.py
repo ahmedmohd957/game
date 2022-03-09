da0ecbea610e0ccc6e91fa42851c123cdbc5a546
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2022-03-09 15:40:37 +0100

    adding graphic table

--- dice_game/highscore.py
+++ dice_game/highscore.py
@@ -1,1 +1,10 @@
-print("hightscore test")
+from dis import pretty_flags
+from prettytable import PrettyTable
+
+
+class HighScore ():
+    columns = ['Players', 'Score', "total"],
+    mytable = PrettyTable()
+    mytable.add_column(columns[0], "Ahmed", "Murwan")
+    mytable.add_column(columns[1], "25", "40")
+    print(mytable)
