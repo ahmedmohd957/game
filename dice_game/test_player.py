718619703d776117d70b11ac5929d17a627bd8af
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2022-03-10 18:23:24 +0100

    adding player test

---
+++ dice_game/test_player.py
@@ -0,0 +1,29 @@
+import unittest
+from dice import Dice
+from player import Player
+
+
+class TestPlayer(unittest.TestCase):
+
+    def test_init_default_object(self):
+        # Instantiate an object.
+        object = Player()
+        message = "given object is not instance of player class."
+        self.assertIsInstance(object, Player, message)
+
+    def setUp(self):
+        #self.p1 = Player.setName(True)
+        self.name = Player.name = "cuz"
+        self.updated_name = Player.updateName = self.name
+
+    def test_Player_name(self):
+        """Test if the name exist"""
+        self.assertEqual(self.name, "cuz")
+
+    def test_Player_name_update(self):
+        """Test if the name updated"""
+        self.assertEqual(self.updated_name, self.name)
+
+
+if __name__ == "__main__":
+    unittest.main()
