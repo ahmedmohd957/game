a919f55fc2e24c71b9d06382af60bb16a4b4b806
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2022-03-11 17:48:15 +0100

    adding game test

--- dice_game/test_game.py
+++ dice_game/test_game.py
@@ -24,12 +24,37 @@ class TestGame(unittest.TestCase):
     def test_start(self):
         # Test the number of player must bee 1 or 2
         self.assertTrue(self.number_of_players == 2 or self.number_of_players == 1)
+        if self.number_of_players == 1:
+            self.p2 = "Computer"
+            self.assertTrue(self.p2 == "Computer")
+        else:
+            self.assertTrue(self.p1 == "Ahmed" and self.p2 == "Cuz")
 
     def test_roll(self):
         # check the roll function
         res = Dice.roll_dice(self)
-        exp = 0 < res >= 6
+        exp = 0 < res <= 6
         self.assertTrue(exp)
+
+    def test_computer(self):
+        # test the computer function
+        if self.number_of_players == 1:
+            self.assertTrue(
+                self.level_of_intelligence == 1 or self.level_of_intelligence == 2
+            )
+
+    def test_score_below_100(self):
+        # test if the score is below 100
+        if self.player1_score < 100 and self.player2_score < 100:
+            self.assertTrue(True)
+        else:
+            self.assertTrue(False)
+
+    def test_cheat(self):
+        # testing the cheat function
+        self.number_of_players = 1
+        self.is_cheating = True
+        self.assertTrue(self.is_cheating)
 
 
 if __name__ == "__main__":
